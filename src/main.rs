446ecaf3b4b26248686f69c9f9d3ff6ac29d2cdb
Simon Bennett <sim.j.bennett@gmail.com>
2023-12-04 14:34:44 +1100

    Added a plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// TODO: Tasks to complete this puzzle 
+    // 1. Take in a text file
+    // 2. Seperate out a line
+    // 3. Look through line for digits
+    // 4. Produce number with first and last digit
+    // 5. Sum all the created numbers
+
 fn main() {
-    println!("Hello, world!");
+
 }
