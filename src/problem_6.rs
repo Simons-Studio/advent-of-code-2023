01be1e829705144489b5dfc047d76cdd777e5e42
Simon Bennett <sim.j.bennett@gmail.com>
2024-01-05 19:55:03 +1100

    create the races from text

--- src/problem_6.rs
+++ src/problem_6.rs
@@ -1,4 +1,4 @@
-use std::{cmp, error::Error, fs};
+use std::{cmp, error::Error, fmt::UpperExp, fs};
 
 use crate::utils::common_ops;
 
@@ -12,6 +12,24 @@ pub fn problem_5() -> Result<(), Box<dyn Error>> {
 struct Race {
     time: i64,
     distance: i64,
+}
+
+fn create_races(input: String) -> Vec<Race> {
+    let mut lines = input.lines();
+    let mut races = Vec::new();
+
+    let times = common_ops::get_numbers(lines.next().unwrap().strip_prefix("Time:").unwrap());
+    let dists = common_ops::get_numbers(lines.next().unwrap().strip_prefix("Distance:").unwrap());
+
+    let time_iter = times.iter();
+    let dist_iter = dists.iter();
+
+    for (&time, &distance) in time_iter.zip(dist_iter) {
+        let race = Race { time, distance };
+        races.push(race);
+    }
+
+    races
 }
 
 fn zeros(race: Race) -> Option<(i64, i64)> {
