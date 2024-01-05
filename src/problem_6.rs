03b572e9e8029b827e86568aed9c27857d05fce7
Simon Bennett <sim.j.bennett@gmail.com>
2024-01-05 19:30:53 +1100

    solutions as zeros

--- src/problem_6.rs
+++ src/problem_6.rs
@@ -1,10 +1,34 @@
-use std::{error::Error, fs};
+use std::{cmp, error::Error, fs};
 
-use crate::utils::{common_ops, interval::Interval};
+use crate::utils::common_ops;
 
 pub fn problem_5() -> Result<(), Box<dyn Error>> {
     let file_path = "./res/06/input";
     let contents = fs::read_to_string(file_path)?;
 
     Ok(())
+}
+
+struct Race {
+    time: i64,
+    distance: i64,
+}
+
+fn zeros(race: Race) -> Option<(i64, i64)> {
+    let f_time = race.time as f64;
+    let f_distance = race.distance as f64;
+
+    let discriminant = f_time * f_time - f_distance;
+
+    if discriminant >= 0.0 {
+        let f_solution_1 = (f_time + discriminant.sqrt()) / 2.0;
+        let f_solution_2 = (f_time - discriminant.sqrt()) / 2.0;
+
+        let min_solution = f_solution_1.min(f_solution_1) as i64;
+        let max_solution = f_solution_1.max(f_solution_2) as i64 + 1;
+
+        Some((min_solution, max_solution))
+    } else {
+        None
+    }
 }
