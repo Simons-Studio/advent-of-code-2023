fd4b9ff5befcdc36c22fc874dcbb27eb4906d4cc
Simon Bennett <sim.j.bennett@gmail.com>
2024-01-05 14:32:57 +1100

    refactor incrementable

--- src/utils/interval.rs
+++ src/utils/interval.rs
@@ -3,7 +3,7 @@ use std::{
     fmt::Display,
 };
 
-use self::incrementable::Incrementable;
+use crate::utils::incrementable::Incrementable;
 
 pub struct Interval<T: Ord + Eq + Display + Copy + Incrementable> {
     start: T,
@@ -79,73 +79,4 @@ pub struct IntervalOverlap<T: Ord + Eq + Display + Copy + Incrementable> {
     left: Option<Interval<T>>,
     overlap: Option<Interval<T>>,
     right: Option<Interval<T>>,
-}
-
-mod incrementable {
-    // Copied from aSpex in
-    // https://stackoverflow.com/questions/41669634/implementing-a-generic-incrementable-trait-in-rust
-    pub trait Incrementable: Copy + std::ops::AddAssign<Self> {
-        fn one() -> Self;
-
-        fn post_inc(&mut self) -> Self {
-            self.post_inc_by(Self::one())
-        }
-
-        fn post_inc_by(&mut self, n: Self) -> Self {
-            let tmp = *self;
-            *self += n;
-            tmp
-        }
-    }
-
-    impl Incrementable for u8 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for u16 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for u32 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for u64 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for i8 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for i16 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for i32 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for i64 {
-        fn one() -> Self {
-            1
-        }
-    }
-    impl Incrementable for f32 {
-        fn one() -> Self {
-            1.0
-        }
-    }
-    impl Incrementable for f64 {
-        fn one() -> Self {
-            1.0
-        }
-    }
 }
